63e01afe65af3385d3ba19b9a496acc2b028fe5e
Marlon Córdova <cordovamarlon33@gmail.com>
2025-12-14 18:36:27 +0100

    Add files via upload

--- triggers/trig_modifica_before.sql
+++ triggers/trig_modifica_before.sql
@@ -1,10 +1,3 @@
-CREATE OR REPLACE TRIGGER trig_modifica_before
-BEFORE UPDATE ON agenda
-FOR EACH ROW
-EXECUTE PROCEDURE trig_modifica_before();
-
-
-
 
 CREATE OR REPLACE FUNCTION trig_modifica_before()
 RETURNS TRIGGER AS $$
@@ -19,8 +12,33 @@ BEGIN
 
       --REQUISITO 2: AUDITAR CAMPO POR CAMPO
       -- Si cambia el NOMBRE
-
+    if NEW.nombre <> OLD.nombre then
+	insert into modificaciones_agenda (codigoagenda,atributo_modificado,valor_antiguo_atributo,valor_nuevo_atributo,fecha_modificacion)
+	values (OLD.idagenda, 'nombre', OLD.nombre, NEW.nombre, current_timestamp);
+    end if;
     if NEW.apellidos <> OLD.apellidos THEN
 
         INSERT INTO modificaciones_agenda (codigoagenda,atributo_modificado,valor_antiguo_atributo,valor_nuevo_atributo,fecha_modicacion)
-	values (OLD.idagenda, 'nombre', OLD.nombre,NEW.nombre,current_timestamp )
+	values (OLD.idagenda, 'apellidos', OLD.apellidos,NEW.apellidos,current_timestamp);
+    end if;
+
+    if NEW.movil <> OLD.movil then
+
+	insert into modificaciones_agenda (codigoagenda, atributo_modificado, valor_antiguo_atributo, valor_nuevo_atributo,fecha_modificacion)
+	values (OLD.idagenda,'movil',OLD.movil,NEW.movil,current_timestamp);
+    end if;
+
+    if NEW.email <> OLD.email then
+
+	insert into modificaciones_agenda (codigoagenda, atributo_modificado, valor_antiguo_atributo, valor_nuevo_atributo, fecha_modificacion)
+	values (OLD.idagenda, 'email',OLD.email, NEW.email, current_timestamp);
+    end if;
+RETURN NEW;
+END;
+$$ language plpgsql;
+
+create or replace trigger trig_modifica_bedore
+before update on agenda
+for each ROW
+execute function trig_modifica_before();
+
